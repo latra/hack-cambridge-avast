ea3e01fe759c3ad6b6fca9ad751b3e0a452c88ac
Oriolac <oriolalascercos@gmail.com>
2019-01-19 19:24:00 +0100

    botó eliminar tot

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 function onoff(){
-    var input = documents.getElementById('on-off-input');
+    var input = document.getElementById("on-off-input");
     if(input.src == "img/on-off.png"){
-
+        input.src = "img/on-off2.png";
     }
 }
