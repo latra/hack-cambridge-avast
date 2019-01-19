e399dad7cee2cd49944b5be56662e9a597bdd984
Josep Antoni <costacamps8@gmail.com>
2019-01-19 20:54:24 +0000

    delete all

--- src/background.js
+++ src/background.js
@@ -1,57 +1,22 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+function onRemoved() {
+  console.log("removed");
+}
 
-function iconClick() {
-  
-  var millisecondsPerDay = 1000 * 60 * 60 * 24 * 1;
-  var oneDayAgo = (new Date()).getTime() - millisecondsPerDay;
-  var sevenDaysAgo = (new Date()).getTime() - (millisecondsPerDay*7);
+function onError(error) {
+  console.error(error);
+}
 
-  chrome.browsingData.remove({
-    "since": sevenDaysAgo,
-    //"till": oneDayAgo // custom for ASB
-  }, {
-      "appcache": true,
-      "cache": true,
-      "cacheStorage": true,
-      "cookies": true,
-      "downloads": true,
-      "fileSystems": true,
-      "formData": true,
-      "history": true,
-      "indexedDB": true,
-      "localStorage": true,
-      "pluginData": true,
-      "passwords": true,
-      "serverBoundCertificates": true,
-      "serviceWorkers": true,
-      "webSQL": true
-  }, function(result) {
-
-    console.log('Clean completed!');
-    console.log(result);
-
-    // send sample JSON to CCleaner results
-    chrome.runtime.getPackageDirectoryEntry(function(root) {
-        root.getFile("report-ccleaner-sample.json", {}, function(fileEntry) {
-          fileEntry.file(function(file) {
-            var reader = new FileReader();
-            reader.onloadend = function(e) {
-              var jsonStr = this.result;
-              var jsonObj = JSON.parse(jsonStr);
-              console.log('Loaded sample object from file...');
-              console.log(jsonObj);
-              chrome.browsingData.reportCleanResults(jsonStr);
-            };
-            reader.readAsText(file);
-          });
-        });
-      });
+function weekInMilliseconds() {
+  return 1000 * 60 * 60 * 24 * 7;
+}
 
 
-  });
+function deleteAll() {
+  var oneWeekAgo = (new Date()).getTime() - weekInMilliseconds();
 
-} // iconClick
-
-window.oncuechange=iconClick;
+  browser.browsingData.remove(
+    {since: oneWeekAgo},
+    {downloads: true, history: true}).
+  then(onRemoved, onError);
+}
--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,54 @@
-function onoff(){
-    var input = document.getElementById("on-off-input");
-    if(input.src == "img/on-off.png"){
-        input.src = "img/on-off2.png";
+var callback = function(){};
+var millisecondsPerMonth = 1000 * 60 * 60 * 24 * 7 * 4;
+
+var oneMonthAgo = (new Date()).getTime() - millisecondsPerMonth;
+
+function clear()
+{
+    if(document.getElementsByName("history").checked)
+    {
+    chrome.browsingData.removeHistory({"since": oneMonthAgo}, callback);
+    }
+    if(document.getElementsByName("cookies").checked)
+    {
+         chrome.browsingData.removeCookies({"since": oneMonthAgo}, callback);
+    }
+    if(document.getElementsByName("cache").checked)
+    {
+        chrome.browsingData.removeCache({"since": oneMonthAgo}, callback);
+    }
+    if(document.getElementsByName("passwords").checked)
+    {
+        chrome.browsingData.removePasswords({"since": oneMonthAgo}, callback);
+    }
+}
+
+function clearAll(Bob)
+{
+    console.log(Bob);
+    chrome.browsingData.removeHistory({"since": oneMonthAgo}, callback);
+    chrome.browsingData.removeCookies({"since": oneMonthAgo}, callback);
+    chrome.browsingData.removeCache({"since": oneMonthAgo}, callback);
+    chrome.browsingData.removePasswords({"since": oneMonthAgo}, callback);
+}
+
+
+var doc = document.getElementById('chosen');
+if(doc)
+{
+    doc.addEventListener('click',clear);
+}
+var doc = document.getElementById('all');
+if(doc)
+{
+    doc.addEventListener('click',clearAll);
+}
+
+
+window.onload=function(){
+    if(document.getElementById("all")){
+        document.getElementById("all").addEventListener('click',function(){
+            clearAll('jaja')
+        });
     }
 }
