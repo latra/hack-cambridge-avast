b1b15821b01f21601fa48736ad8e365300739180
Oriolac <oriolalascercos@gmail.com>
2019-01-19 18:45:11 +0100

    Task2

--- src/popup.js
+++ src/popup.js
@@ -2,6 +2,7 @@ function onoff(){
     var image = document.getElementById("imgOnOffButton");
     if(image.src == "img/on-off.png"){
         image.src = "img/on-off2.png";
+        document.getElementById("h1id").textContent = "yourTextHere";
     } else {
         image.src = "img/on-off.png";
     }
