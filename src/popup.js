0fd12ab8d9f1e7e5eb11ee191d2c1d9d0c4bd9a6
Latra <Personadelmonton@gmail.com>
2019-01-20 01:30:57 +0100

    Oriol lo ha hecho mejor.

--- src/popup.js
+++ src/popup.js
@@ -3,29 +3,31 @@ var millisecondsPerMonth = 1000 * 60 * 60 * 24 * 7 * 4;
 
 var oneMonthAgo = (new Date()).getTime() - millisecondsPerMonth;
 
+
 function clear()
 {
-    if(document.getElementsByName("history").checked)
+    document.getElementById('test').innerHTML = document.getElementById("history").checked;
+    if(document.getElementById("history").checked)
     {
-    chrome.browsingData.removeHistory({"since": oneMonthAgo}, callback);
+        chrome.browsingData.removeHistory({"since": oneMonthAgo}, callback);
     }
-    if(document.getElementsByName("cookies").checked)
+    if(document.getElementById("cookies").checked)
     {
          chrome.browsingData.removeCookies({"since": oneMonthAgo}, callback);
     }
-    if(document.getElementsByName("cache").checked)
+    if(document.getElementById("cache").checked)
     {
         chrome.browsingData.removeCache({"since": oneMonthAgo}, callback);
     }
-    if(document.getElementsByName("passwords").checked)
+    if(document.getElementById("passwords").checked)
     {
         chrome.browsingData.removePasswords({"since": oneMonthAgo}, callback);
     }
 }
 
-function clearAll(Bob)
+function clearAll()
 {
-    console.log(Bob);
+    console.log();
     chrome.browsingData.removeHistory({"since": oneMonthAgo}, callback);
     chrome.browsingData.removeCookies({"since": oneMonthAgo}, callback);
     chrome.browsingData.removeCache({"since": oneMonthAgo}, callback);
@@ -46,9 +48,34 @@ if(doc)
 
 
 window.onload=function(){
+    if(document.getElementById("chosen")){
+        document.getElementById("chosen").addEventListener('click', function(){
+            clear();
+        });
+    }
     if(document.getElementById("all")){
-        document.getElementById("all").addEventListener('click',function(){
-            clearAll('jaja')
+        document.getElementById("all").addEventListener('click', function(){
+            clearAll();
+        });
+    }
+    if(document.getElementById("history")){
+        document.getElementById("history").addEventListener('click', function(){
+            document.getElementById("history").checked = document.getElementById("history").checked;
+        });
+    }
+    if(document.getElementById("cookies")){
+        document.getElementById("cookies").addEventListener('click', function(){
+            document.getElementById("cookies").checked = document.getElementById("cookies").checked;
+        });
+    }
+    if(document.getElementById("cache")){
+        document.getElementById("cache").addEventListener('click', function(){
+            document.getElementById("cache").checked = document.getElementById("cache").checked;
+        });
+    }
+    if(document.getElementById("passwords")){
+        document.getElementById("passwords").addEventListener('click', function(){
+            document.getElementById("passwords").checked = document.getElementById("passwords").checked;
         });
     }
 }
